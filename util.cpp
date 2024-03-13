86df79b10ee5cab3c3746ea513353c1d96ff175e
jjyj5264 <jjyj5264@gmail.com>
2024-03-13 22:26:26 +0900

    Added initializeSeed() and getRandomAnswer(), which returns answer.

--- util.cpp
+++ util.cpp
@@ -1,2 +1,10 @@
-#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+void initializeSeed() {
+    std::srand(std::time(0));
+}
+
+int getRandomAnswer() {
+    return std::rand() % 1000; // 000 - 999
+}
