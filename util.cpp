b471f01b1b9b9dc23e0427ca8e11669489d7d3f3
jjyj5264 <jjyj5264@gmail.com>
2024-03-13 23:08:16 +0900

    Modified getRandomAnswer() to output with non-repeating digits.

--- util.cpp
+++ util.cpp
@@ -1,10 +1,26 @@
 #include <cstdlib>
 #include <ctime>
 
+// Current Version: v2
+
 void initializeSeed() {
     std::srand(std::time(0));
 }
 
 int getRandomAnswer() {
-    return std::rand() % 1000; // 000 - 999
+    int first = 0;
+    int second = 0;
+    int third = 0;
+
+    while (true) {
+        first = std::rand() % 10; // 0 - 9
+        second = std::rand() % 10;
+        third = std::rand() % 10;
+
+        if (!(first == second || first == third || second == third)) {
+            break;
+        }
+    }
+
+    return (third * 100) + (second * 10) + first;
 }
